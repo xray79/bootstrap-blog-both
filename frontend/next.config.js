03a8441180c7a5b093c135470871fb156c7409a8
Ibnul Huq <ibnul.huq@gmail.com>
2022-09-11 11:08:08 +0100

    complete initial setup

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: { ignoreBuildErrors: true },
+};
